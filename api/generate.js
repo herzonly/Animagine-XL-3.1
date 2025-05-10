cfdb94528e94ba85b7423ad10e9acda2256816a7
justme_herza <157363409+herzonly@users.noreply.github.com>
2025-05-11 07:04:55 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,6 +1,6 @@
-const { client } = require('@gradio/client');
+import { client } from '@gradio/client';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
@@ -64,4 +64,4 @@ module.exports = async (req, res) => {
       details: error.message 
     });
   }
-};
+}
